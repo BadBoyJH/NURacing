b8577b4a0d1aaf3ec36cb62eb4a52ae2a2c4ca83
BadBoyJH <james.hibbard@westnet.com.au>
2013-09-08 23:03:31 +1000

    Change to Email System, made more centralised, added .Settings file, more User functionality.

---
+++ BusinessLogicLayer/EmailManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace BusinessLogicLayer
+{
+    class EmailManager
+    {
+        private static string ourEmail = "NuRacingTest@westnet.com.au";
+        private static string ourPassword = "CaJaMiNiSaSiTo3930";
+        private static SmtpClient smtp;
+
+        static EmailManager()
+        {
+            smtp = new SmtpClient("mail.westnet.com.au");
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential(ourEmail, ourPassword);
+        }
+
+        static public void SendPasswordResetRequest(byte[] ByteCode, string recipient)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in ByteCode)
+            {
+                builder.Append(b.ToString("X2"));
+            }
+
+            string HexCode = builder.ToString();
+
+            string message =
+                "Until further notice this will have to do\n" +
+                "Bytecode is: " + HexCode + "\n" +
+                "NURacing Test";
+
+            MailMessage email = new MailMessage();
+            email.To.Add(recipient);
+            email.Subject = "NURacing Online - Password Reset";
+            email.From = new MailAddress(ourEmail);
+            email.Body = message;
+
+            smtp.Send(email);
+        }
+
+
+
+        static public void SendPasswordResetRequest(byte[] ByteCode, string recipient)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in ByteCode)
+            {
+                builder.Append(b.ToString("X2"));
+            }
+
+            string HexCode = builder.ToString();
+
+            string message =
+                "Until further notice this will have to do\n" +
+                "New Password is: " + HexCode + "\n" +
+                "NURacing Test";
+
+            MailMessage email = new MailMessage();
+            email.To.Add(recipient);
+            email.Subject = "NURacing Online - Password Reset Complete";
+            email.From = new MailAddress(ourEmail);
+            email.Body = message;
+
+            smtp.Send(email);
+        }
+    }
+}
---
+++ NURacingWebsite/NURacingMembershipProvider.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace NURacingWebsite
+{
+    public class NURacingMembershipProvider //: MembershipProvider
+    {/*
+        public override bool EnablePasswordReset
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get 
+            { 
+                return 3; 
+            }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get 
+            { 
+                return 15;
+            }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get 
+            {
+                return MembershipPasswordFormat.Hashed;
+            }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get 
+            {
+                return BusinessLogicLayer.User.passwordMinNonAlphaNumericChars;
+            }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get 
+            {
+                return BusinessLogicLayer.User.passwordMinLength;
+            }
+        }
+
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get 
+            {
+                return BusinessLogicLayer.User.passwordRegEx;
+            }
+        }*/
+    }
+}
