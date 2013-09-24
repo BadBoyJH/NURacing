90c78a255c2f468f828f09dfaa182b3e4cb8bd07
cheesestringer <cheesestringer@hotmail.com>
2013-09-25 00:38:30 +1000

    TaskInfo and AssignedTasks classes created

---
+++ BusinessLogicLayer/AssignedTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    class AssignedTask
+    {
+        /// <summary>
+        /// Adds a task for the specified user.
+        /// </summary>
+        /// <param name="assigningUser"></param>
+        /// <param name="assignedToUser"></param>
+        /// <param name="workType"></param>
+        /// <param name="name"></param>
+        /// <param name="description"></param>
+        /// <param name="takeFiveNeeded"></param>
+        
+        static public void addTask(string assigningUser, string assignedToUser, int workType, string name, string description, bool takeFiveNeeded)
+        {
+            
+        }
+    }
+}
---
+++ BusinessLogicLayer/TaskInfo.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataAccessLayer;
+using DataAccessLayer.NuRacingDataSetTableAdapters;
+
+//Written By Simon Davis
+
+namespace BusinessLogicLayer
+{
+    class TaskInfo
+    {
+        //Task Details
+        private UserInfo assigningUserInfo;
+
+        private UserInfo userAssignedInfo;
+
+        private int workTypeID;
+
+        private string taskName;
+
+        private string taskDescription;
+
+        private bool takeFiveNeeded;
+
+        private string taskStatus;
+
+        private string taskIncompleteReason;
+
+        public UserInfo AssigningUserInfo
+        { 
+            get
+            {
+                return assigningUserInfo;
+            }
+        }
+
+        public UserInfo UserAssignedInfo
+        {
+            get
+            {
+                return userAssignedInfo;
+            }
+        }
+
+        public int WorkTypeID
+        {
+            get
+            {
+                return workTypeID;
+            }
+        }
+
+        public string TaskName
+        {
+            get
+            {
+                return taskName;
+            }
+        }
+
+        public string TaskDescription
+        {
+            get
+            {
+                return taskDescription;
+            }
+        }
+
+        public bool TakeFiveNeeded
+        {
+            get
+            {
+                return takeFiveNeeded;
+            }
+        }
+
+        public string TaskStatus
+        {
+            get
+            {
+                return taskStatus;
+            }
+        }
+
+        public string TaskIncompleteReason
+        {
+            get
+            {
+                return taskIncompleteReason;
+            }
+        }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="taskRow">A row from the assigned tasks table</param>
+
+        private TaskInfo(NuRacingDataSet.assignedtaskRow taskRow)
+        {
+            assigningUserInfo = UserInfo.getUser(taskRow.User_Username_AssignedBy);
+
+            userAssignedInfo = UserInfo.getUser(taskRow.User_Username_AssignedTo);
+
+            workTypeID = taskRow.WorkType_UID;
+
+            taskName = taskRow.Task_Name;
+
+            taskDescription = taskRow.Task_Description;
+
+            takeFiveNeeded = taskRow.Task_TakeFiveNeeded;
+
+            taskStatus = taskRow.Task_Status;
+
+            taskIncompleteReason = taskRow.Task_IncompleteReason;
+        }
+
+        static TaskInfo getAssignedTask(int assignedTaskID)
+        {
+            assignedtaskTableAdapter assignedTaskAdapter = new assignedtaskTableAdapter();
+
+            NuRacingDataSet.assignedtaskDataTable assignedTaskTable = assignedTaskAdapter.GetAssignedTask(assignedTaskID);
+
+            if (assignedTaskTable.Rows.Count == 0)
+            {
+                throw new ArgumentException("Assigned Task Doesn't Exist");
+            }
+
+            return new TaskInfo((NuRacingDataSet.assignedtaskRow)assignedTaskTable.Rows[0]);
+        }
+
+        static List<TaskInfo> getWorkTypeTasks(int workID)
+        {
+            assignedtaskTableAdapter assignedTaskAdapter = new assignedtaskTableAdapter();
+
+            NuRacingDataSet.assignedtaskDataTable assignedTaskTable = assignedTaskAdapter.GetDataByWorkTypeID(workID);
+
+            List<TaskInfo> result = new List<TaskInfo>();
+
+            foreach (NuRacingDataSet.assignedtaskRow AssignedTaskRow in assignedTaskTable.Rows)
+            {
+                result.Add(new TaskInfo(AssignedTaskRow));
+            }
+
+            return result;
+        }
+
+        //Waiting on James to add required methods
+
+        //static List<TaskInfo> getProjectTasks(int projectID)
+        //{
+        //    worktypeTableAdapter workTypeAdapter = new worktypeTableAdapter();
+
+        //    NuRacingDataSet.worktypeDataTable workTypeTable = workTypeAdapter.GetDataByProjectID(projectID);
+
+        //    //List<TaskInfo> taskList = new
+
+        //    return 
+        //}
+    }
+}
