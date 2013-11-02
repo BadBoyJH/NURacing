046d0d99830fd976c529b7cceeae889db4b91552
James Hibbard <James.hibbard@westnet.com.au>
2013-11-02 18:47:17 +1100

    Reverted to DB4Free.net no more speed for you!

--- DataAccessLayer/Properties/Settings.Designer.cs
+++ DataAccessLayer/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace DataAccessLayer.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("server=192.168.1.6;User Id=webaccessaccount;password=MaskHipsterHeadphonesChopsti" +
+        [global::System.Configuration.DefaultSettingValueAttribute("server=db4free.net;User Id=webaccessaccount;password=MaskHipsterHeadphonesChopsti" +
             "cks;Persist Security Info=True;database=nuracing")]
         public string NuRacingConnectionString {
             get {
